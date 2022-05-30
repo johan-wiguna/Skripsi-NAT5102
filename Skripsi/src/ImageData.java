a4ea883ed6426f3647d24e7a1727b0ec4ef13a06
Johan Adi Wiguna <i-johan.wiguna@dana.id>
2022-05-30 18:52:17 +0700

    Added class path

--- Skripsi/src/ImageData.java
+++ Skripsi/src/ImageData.java
@@ -9,16 +9,32 @@ public class ImageData {
     Mat image;
     Mat descriptor;
     MatOfKeyPoint keypoints;
+    double[] bounds;
+    double[][] segmentRange;
     
     public ImageData(String path, boolean type, int index) {
         this.path = path;
         this.type = type;
+        this.index = index;
         keypoints = new MatOfKeyPoint();
         descriptor = new Mat();
     }
 
     public String getPath() {
         return path;
+    }
+    
+    public String getClassPath() {
+        String classPath = "";
+        
+        for (int i = path.length() - 1; i >= 0; i--) {
+            if (path.charAt(i) == '\\') {
+                classPath = path.substring(0, i);
+                break;
+            }
+        }
+        
+        return classPath;
     }
 
     public boolean isType() {
@@ -41,6 +57,10 @@ public class ImageData {
         return keypoints;
     }
     
+    public void setIndex(int index) {
+        this.index = index;
+    }
+    
     public void detectKeypoints() {
         image = Imgcodecs.imread(path, Imgcodecs.IMREAD_GRAYSCALE);
         
@@ -53,5 +73,44 @@ public class ImageData {
         
         SIFT sift = SIFT.create(keypointAmount);
         sift.detectAndCompute(image, new Mat(), keypoints, descriptor);
+    }
+    
+    public void segmentImage() {
+        KeyPoint[] keypoints = this.keypoints.toArray();
+        bounds = new double[4];
+        double topBound = Double.MIN_VALUE;
+        double bottomBound = Double.MAX_VALUE;
+        double leftBound = Double.MAX_VALUE;
+        double rightBound = Double.MIN_VALUE;
+        
+        for (KeyPoint keypoint : keypoints) {
+            if (keypoint.pt.y > topBound) {
+                topBound = keypoint.pt.y;
+            }
+            if (keypoint.pt.y < bottomBound) {
+                bottomBound = keypoint.pt.y;
+            }
+            if (keypoint.pt.x < leftBound) {
+                leftBound = keypoint.pt.x;
+            }
+            if (keypoint.pt.x > rightBound) {
+                rightBound = keypoint.pt.x;
+            }
+        }
+        
+        bounds[0] = topBound;
+        bounds[1] = bottomBound;
+        bounds[2] = leftBound;
+        bounds[3] = rightBound;
+        
+        double newWidth = rightBound - leftBound;
+        double segmentWidth = newWidth / 4;
+        
+        segmentRange = new double[4][2];
+        
+        for (int i = 0; i < 4; i++) {
+            segmentRange[i][0] = segmentWidth * i + leftBound;
+            segmentRange[i][1] = segmentWidth * (i + 1) + leftBound;
+        }
     }
 }
